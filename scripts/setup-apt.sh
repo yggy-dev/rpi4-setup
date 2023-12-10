757578efe575848185ce4c966e6c3ae3546e14e8
Eric Otto <mail@ericotto.dev>
2023-12-10 02:13:46 +0100

    🐛 FIX: apt

--- scripts/setup-apt.sh
+++ scripts/setup-apt.sh
@@ -3,4 +3,4 @@
 # Install or update apt-packages
 
 sudo apt update && sudo apt full-upgrade
-sudo apt install apt-transport-https avahi-daemon ca-certificates curl gnupg-agent software-properties-common gcc --install-recommends --install-suggests --fix-broken --fix-missing --reinstall --purge --auto-remove
+sudo apt install apt-transport-https avahi-daemon ca-certificates curl gnupg-agent software-properties-common gcc --install-recommends --fix-broken --fix-missing --reinstall --purge --auto-remove
