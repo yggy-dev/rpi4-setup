50082bb643f9ddb6c62bd7ab2b7e8e2c4c400739
Eric Otto <mail@ericotto.dev>
2023-12-10 02:50:38 +0100

    🐛 FIX: CWD

--- scripts/setup-boot.sh
+++ scripts/setup-boot.sh
@@ -2,9 +2,9 @@
 
 # REPLACE /boot.conf with ../configs/boot.conf
 
-sudo sed -i 's/#dtparam=i2c_arm=on/dtparam=i2c_arm=on/g' /boot.conf
-sudo sed -i '/dtparam=i2c_arm=on/a\dtparam=i2c_arm_baudrate=400000' /boot.conf
-sudo sed -i '/#dtparam=spi=on/a\dtoverlay=gpio-shutdown,gpio_pin=4,active_low=1,gpio_pull=up' /boot.conf
+sudo sed -i 's/#dtparam=i2c_arm=on/dtparam=i2c_arm=on/g' ./boot.conf
+sudo sed -i '/dtparam=i2c_arm=on/a\dtparam=i2c_arm_baudrate=400000' ./boot.conf
+sudo sed -i '/#dtparam=spi=on/a\dtoverlay=gpio-shutdown,gpio_pin=4,active_low=1,gpio_pull=up' ./boot.conf
 
 ### ENABLE i2c IN FIRMWARE ###
 sudo sed -i 's/#dtparam=i2c_arm=on/dtparam=i2c_arm=on/g' /boot/firmware/config.txt
