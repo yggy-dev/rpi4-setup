84129f72efe2c959a3deddd39aafff7c09cb79ee
Eric Otto <mail@ericotto.dev>
2023-12-10 02:26:11 +0100

    🐛 FIX: lcd

--- scripts/setup-lcd_display.sh
+++ scripts/setup-lcd_display.sh
@@ -17,7 +17,7 @@ sudo rm -rf SKU_RM0004
 # ADD TO /etc/rc.local
 # sudo sed -i '/exit 0/i\/usr/local/bin/display &' /etc/rc.local
 # OR TO /usr/lib/systemd/system/
-sudo cp ../lcd_display.service /usr/lib/systemd/system/lcd_display.service
+sudo cp /lcd_display.service /usr/lib/systemd/system/lcd_display.service
 cd /usr/lib/systemd/system/
 sudo chmod +x lcd_display.service
 sudo systemctl daemon-reload
