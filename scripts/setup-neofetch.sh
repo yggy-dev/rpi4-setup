bf73bc2c7f2202f9c5e7833405db48e18f4efd2f
Eric Otto <mail@yggy.dev>
2024-05-10 02:31:46 +0200

    🐛 FIX: remove neofetch

--- scripts/setup-neofetch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install and setup neofetch
-
-set -euo pipefail
-
-sudo apt install neofetch
-sudo cp ./scripts/motd.sh /etc/profile.d/motd.sh
-sudo chmod +x /etc/profile.d/motd.sh
--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo bash ./scripts/setup-apt.sh
-sudo bash ./scripts/setup-neofetch.sh
 sudo bash ./scripts/setup-boot.sh
 sudo bash ./scripts/setup-lcd_display.sh
 
